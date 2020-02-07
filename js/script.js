03812f86771be2ef2b8737b99c7d58cdd027fe8a
Federico <federico.caiello@gmail.com>
2020-02-07 16:20:48 +0100

    voto con stelle

--- js/script.js
+++ js/script.js
@@ -36,44 +36,34 @@ function printFilms (films) {
 
   for (var i = 0; i < films.length; i++) {
      var thisFilms = films[i];
-     // console.log(thisFilms);
+     voto = getRandomInclusive(1, 5);
+
      var context = {
        title: thisFilms.title,
        original_title: thisFilms.original_title,
        original_language: thisFilms.original_language,
-       vote_average: thisFilms.vote_average
+       vote_average: printStars(thisFilms.vote_average),
+
      };
      var html = template(context);
      $('.cover').append(html);
   }
 }
-
-// 'title': "Back to the Future",
-// "original_title": "Back to the Future",
-// "original_language": "en",
-// "vote_average": 8.2,
-
-// "page": 1,
-//     "total_results": 3,
-//     "total_pages": 1,
-//     "results": [
-//         {
-//             "popularity": 24.954,
-//             "vote_count": 12242,
-//             "video": false,
-//             "poster_path": "/pTpxQB1N0waaSc3OSn0e9oc8kx9.jpg",
-//             "id": 105,
-//             "adult": false,
-//             "backdrop_path": "/x4N74cycZvKu5k3KDERJay4ajR3.jpg",
-//             "original_language": "en",
-//             "original_title": "Back to the Future",
-//             "genre_ids": [
-//                 12,
-//                 35,
-//                 878,
-//                 10751
-//             ],
-//             "title": "Back to the Future",
-//             "vote_average": 8.2,
-//             "overview": "Eighties teenager Marty McFly is accidentally sent back in time to 1955, inadvertently disrupting his parents' first meeting and attracting his mother's romantic interest. Marty must repair the damage to history by rekindling his parents' romance and - with the help of his eccentric inventor friend Doc Brown - return to 1985.",
-//             "release_date": "1985-07-03"
+function getRandomInclusive (min, max) {
+  min = Math.ceil(min) ;
+  max = Math.floor(max) ;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function printStars(voto) {
+  var voto = Math.round(voto / 2);
+  var stelle = '';
+  for (var i=0; i<=5; i++) {
+    if (i <= voto) {
+      var singolaStella = '<i class="fas fa-star"></i>';
+    }else {
+      var singolaStella = '<i class="far fa-star"></i>';
+    }
+    stelle += singolaStella;
+  }
+  return stelle;
+}

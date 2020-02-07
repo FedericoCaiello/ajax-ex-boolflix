16fa7ffe784c95cc2517ed6f79c0e2f40b95e0c0
Federico <federico.caiello@gmail.com>
2020-02-07 18:49:56 +0100

    nuova chiamata ajax con funzione per la serieTv

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function (){
             console.log(films);
             printFilms(films);
             if(films.length == 0){
-              alert('la tua richiesta non ha prodotto risultati');
+              // alert('la tua richiesta non ha prodotto risultati');
             }
 
           },
@@ -25,6 +25,28 @@ $(document).ready(function (){
             console.log(errors);
           }
         });
+        $.ajax(
+          {
+            url: 'https://api.themoviedb.org/3/search/tv',
+            method: 'GET',
+            data: {
+              api_key: 'ed6b74196763d1becc194df37b95d2fd',
+              query: query
+            },
+            success: function (data) {
+              console.log(data);
+              var serieTv = data.results;
+              console.log(serieTv);
+              printSerieTv(serieTv);
+              if(serieTv.length == 0) {
+                // alert('non sono stati trovati serieTv');
+              }
+            },
+            error: function (request, state, errors) {
+              console.log(errors);
+            }
+          }
+        );
     }
   });
 });
@@ -36,12 +58,11 @@ function printFilms (films) {
 
   for (var i = 0; i < films.length; i++) {
      var thisFilms = films[i];
-     voto = getRandomInclusive(1, 5);
 
      var context = {
        title: thisFilms.title,
        original_title: thisFilms.original_title,
-       original_language: thisFilms.original_language,
+       original_language:'img/flag-of-' + thisFilms.original_language + '.png',
        vote_average: printStars(thisFilms.vote_average),
 
      };
@@ -49,15 +70,28 @@ function printFilms (films) {
      $('.cover').append(html);
   }
 }
-function getRandomInclusive (min, max) {
-  min = Math.ceil(min) ;
-  max = Math.floor(max) ;
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function printSerieTv (serie) {
+  $('.cover').html('');
+  var source = $("#second-template").html();
+  var template = Handlebars.compile(source);
+  for (var i = 0; i < serie.length; i++) {
+     var questaSerie = serie[i];
+
+     var context = {
+       name: questaSerie.name,
+       original_name: questaSerie.original_name,
+       original_language:'img/flag-of-' + questaSerie.original_language + '.png',
+       vote_average: printStars(questaSerie.vote_average)
+     };
+     var html = template(context);
+     $('.cover').append(html);
+  }
 }
+
 function printStars(voto) {
   var voto = Math.round(voto / 2);
   var stelle = '';
-  for (var i=0; i<=5; i++) {
+  for (var i=1; i<=5; i++) {
     if (i <= voto) {
       var singolaStella = '<i class="fas fa-star"></i>';
     }else {
